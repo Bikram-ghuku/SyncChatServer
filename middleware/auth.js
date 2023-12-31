e1dd5889ccf0db9e89a14932d850d13afa57b468
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-31 14:21:15 +0530

    added authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const authentication = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+        const data = jwt.verify(token, process.env.TOKEN);
+        req.user = data;
+        next();
+    } catch (error) {
+        res.status(401).json({message: 'Invalid token'});
+    }
+}
