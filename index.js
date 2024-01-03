ad9d28e81b6e27175096bccd782ec75149a006f5
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-03 21:39:46 +0530

    added socket io and added socket io service

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const SocketService = require('./services/socket')
+const http = require('http')
 
 const userRoutes = require('./routes/userRoutes')
 const channelRoutes = require('./routes/channelRoutes')
 
 const server = express();
+const httpServer = http.createServer(server)
+const socketService = new SocketService();
+
+socketService.init(httpServer);
+socketService.initListeners();
+
 server.use(cors());
 server.use(bodyParser.json());
 
 server.use('/users', userRoutes)
 server.use('/channels', channelRoutes)
 
-server.listen(process.env.PORT || 3030, () => {
-    console.log('Server started on port:', process.env.PORT || 3030);
+
+httpServer.listen(process.env.PORT || 3001, () => {
+    console.log('Socket server started on port:', process.env.PORT || 3001);
 });
