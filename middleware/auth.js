1fe792191c921c426708c7df0f1811330067dd24
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-11 21:57:43 +0530

    chore: fox auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
     try {
+        const token = req.headers.authorization.split(' ')[1];
         const data = jwt.verify(token, process.env.TOKEN);
-        req.user = data;
+        req.user = data
         next();
     } catch (error) {
         res.status(401).json({message: 'Invalid token'});
