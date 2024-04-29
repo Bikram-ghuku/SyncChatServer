ee5cf3dbaec04c02634c1fa8ebcda539c1bf3a0b
Bikram Ghuku <bikramghuku05@gmail.com>
2024-04-29 11:00:50 +0530

    feat: added add chat route and controller

--- controllers/channels.js
+++ controllers/channels.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const { DbService } = require('../services/db')
+const db = new DbService()
 
 const chats1 = [
     {
@@ -11,7 +13,7 @@ const chats1 = [
 
 const getChannels = (req, res) => {
     const chats = JSON.parse(JSON.stringify(chats1))
-    const email = req.user.name
+    const email = req.body.user.name
     const channels = chats.filter(chat => chat.name.includes(email))
     channels.forEach((data) => {
         var newChannel = []
@@ -23,4 +25,12 @@ const getChannels = (req, res) => {
     res.json(channels)
 }
 
-module.exports = {getChannels}
+const addChannels = async (req, res) => {
+	const {userId, email, name} = await db.getUser(req.body.email)
+	req.body.user2 = {userId, email, name}
+	const ret = await db.createChat(req.body)
+	if (ret) return res.status(201).json({message: 'Creation success'})
+	else return res.status(409).json({message: 'Chat already exists'})
+}
+
+module.exports = { getChannels, addChannels }
--- routes/channelRoutes.js
+++ routes/channelRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const {getChannels} = require('../controllers/channels');
+const { getChannels, addChannels } = require('../controllers/channels');
 const authenticateToken = require('../middleware/auth');
 
 const router = express.Router();
 router.get('/channels', authenticateToken, getChannels);
+router.post('/addChannels', authenticateToken, addChannels);
 module.exports = router;
