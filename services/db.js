eab3d92afdfcf84fc96d1e531ab0d02cca68ada9
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-07 22:33:38 +0530

    feat: set isread to true upon requesting message

--- services/db.js
+++ services/db.js
@@ -1,15 +1,9 @@
 const { PrismaClient } = require('@prisma/client')
 
-var db = null
 
 class DbService{
-    db = null
     constructor(){
-        if(db) this.db = db;
-        else{
-            db = new PrismaClient()
-            this.db = db
-        }
+        this.db = new PrismaClient()
     }
 
     async addUser(userData){
@@ -140,6 +134,18 @@ class DbService{
             lineData.isRead = x[i].IsRead
             res.push(lineData)
         }
+        await this.db.messages.updateMany({
+            where: {
+                ChanId: chanId,
+                NOT: {
+                    senderId: userId
+                }
+            },
+            data:{
+                IsRead: true
+            }
+        })
+
         return res
     }
 }
